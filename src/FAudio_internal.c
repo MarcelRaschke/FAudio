MarcelRaschke/FAudio

commit 710ff751b1b37ab8b35011c15272d19e204898fe

faudio: Defer clearing fields at EOS until after resampling.
XAUDIO2_END_OF_STREAM indicates that EOS should happen after the current buffer
completes, but we still need to resample these last few samples.

This fixes a regression introduced by 5acd7526e2c0ada56304fbc79c1a22a976d8cbe9.

Additionally, totalSamples as it is reported by IXAudio2SourceVoice::GetState()
should not report zero from OnBufferEnd() / OnStreamEnd(). This discrepancy was
present even before the mentioned commit.

src/FAudio_internal.c
@@ -535,11 +535,7 @@ static void end_buffer(FAudioSourceVoice *voice)
 #endif /* HAVE_WMADEC */
 
 	if (eos)
-	{
-		voice->src.resampleOffset = 0;
-		voice->src.totalSamples = 0;
-		FAudio_memset(voice->src.resample_taps, 0, sizeof(voice->src.resample_taps));
-	}
+		voice->src.eos = true;
 
 	LOG_INFO(voice->audio, "Voice %p, finished with buffer %p", voice, buffer)
 
@@ -1205,6 +1201,14 @@ static void FAudio_INTERNAL_MixSource(FAudioSourceVoice *voice)
 			voice->src.resample_taps[0][i] = voice->audio->decoded_audio[toDecode - 2 * channels + i];
 			voice->src.resample_taps[1][i] = voice->audio->decoded_audio[toDecode - channels + i];
 		}
+	}
+
+	if (voice->src.eos)
+	{
+		voice->src.totalSamples = 0;
+		voice->src.resampleOffset = 0;
+		FAudio_memset(voice->src.resample_taps, 0, sizeof(voice->src.resample_taps));
+		voice->src.eos = false;
 	}
 
 	/* Done with buffers, finally. */

src/FAudio_internal.h
@@ -537,6 +537,7 @@ struct FAudioVoice
 
 			/* Dynamic */
 			uint8_t active;
+			bool eos;
 			float freqRatio;
 			uint64_t totalSamples;
 

commit e1b810a5c849ea6d88aec13c7d3f99067b3c269c

faudio: Handle deferred EOS also if no samples were decoded.
This fixes a regression introduced by 710ff751b1b37ab8b35011c15272d19e204898fe
and visible in the Wine tests.

src/FAudio_internal.c
@@ -1111,6 +1111,14 @@ static void FAudio_INTERNAL_MixSource(FAudioSourceVoice *voice)
 	/* Nothing to resample? */
 	if (toDecode == 0)
 	{
+		if (voice->src.eos)
+		{
+			voice->src.totalSamples = 0;
+			voice->src.resampleOffset = 0;
+			FAudio_memset(voice->src.resample_taps, 0, sizeof(voice->src.resample_taps));
+			voice->src.eos = false;
+		}
+
 		FAudio_PlatformUnlockMutex(voice->src.bufferLock);
 		LOG_MUTEX_UNLOCK(voice->audio, voice->src.bufferLock)
 